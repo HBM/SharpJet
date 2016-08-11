726bdb4ca09316930fef856cdf9cff7234b26818
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-08-11 15:47:29 +0200

    Correct AssemblyInformation.

--- SharpJet/Properties/AssemblyInfo.cs
+++ SharpJet/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cs-jet")]
+[assembly: AssemblyTitle("SharpJet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
