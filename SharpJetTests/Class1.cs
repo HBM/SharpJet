1cae9a6438b90c44492d522e10694cb61ed81326
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-08-11 13:23:17 +0200

    Add unit test sample project.

---
+++ SharpJetTests/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Hbm.Devices.Jet
+{
+    using NUnit.Framework;
+
+    public class TestClass
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
