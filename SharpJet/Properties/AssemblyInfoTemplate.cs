06f9fec71009869939a85cb0ee79a5e25ad87a56
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-08-13 14:15:30 +0200

    Add version info in AssemblyInfo.cs by prebuild script.

--- SharpJet/Properties/AssemblyInfoTemplate.cs
+++ SharpJet/Properties/AssemblyInfoTemplate.cs
@@ -38,9 +38,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpJet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cs-jet")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Hottinger Baldwin Messtechnik GmbH")]
+[assembly: AssemblyProduct("SharpJet")]
+[assembly: AssemblyCopyright("Copyright © Hottinger Baldwin Messtechnik GmbH 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -62,5 +62,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0%revcount%%shorthash%%dirty%")]
---
+++ SharpJet/scripts/version.ps1
@@ -0,0 +1,35 @@
+param(
+	[string]$git = "git",
+	[string]$solutiondir = "solutiondir",
+	[string]$assembly_info_template = "assembly_info_template",
+	[string]$assembly_info = "assembly_info"
+)
+
+[System.Environment]::CurrentDirectory = $solutiondir
+
+$rev_count = & $git rev-list HEAD --count
+$dirty_string = & $git diff --shortstat
+$is_dirty = $FALSE
+if ($dirty_string) { $is_dirty = $TRUE }
+$git_hash = & $git rev-parse --verify HEAD
+$git_short_hash = & $git rev-parse --verify --short HEAD
+& $git describe --exact-match --tags HEAD
+$git_tag = $LastExitCode
+$is_tag = $git_tag -and !($is_dirty)
+
+$template_content = Get-Content -Path $assembly_info_template
+if ($is_tag) {
+	$template_content = $template_content -replace "%revcount%", ""
+	$template_content = $template_content -replace "%shorthash%", ""
+	$template_content = $template_content -replace "%dirty%", ""
+} else {
+	$template_content = $template_content -replace "%revcount%", -$rev_count
+	$template_content = $template_content -replace "%shorthash%", "+$git_short_hash"
+	if ($is_dirty) {
+		$template_content = $template_content -replace "%dirty%", '.dirty'
+	} else {
+		$template_content = $template_content -replace "%dirty%", ""
+	}
+}
+
+$template_content | Out-File $assembly_info
