d16207c6150fec64e666a953df866b0e1bbcc5fa
Jens Eisenbach <jens.eisenbach@hbm.com>
2017-04-28 10:29:28 +0200

    Added unit tests for JetFetcher.
    Installed FakeItEasy mocking framework via nuget. This is used to fake a System.Action and intercept its invocation.

---
+++ SharpJetTests/JetFetcherTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SharpJetTests
+{
+    using System;
+    using FakeItEasy;
+    using Hbm.Devices.Jet;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JetFetcherTests
+    {
+        [Test]
+        public void TestCallFetchCallbackInvokesAction()
+        {
+            Action<JToken> callback = A.Fake<Action<JToken>>();
+            JetFetcher fetcher = new JetFetcher(callback);
+            JToken token = JToken.FromObject(42);
+
+            fetcher.CallFetchCallback(token);
+
+            A.CallTo(() => callback.Invoke(token)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Test]
+        public void TestCallFetchCallbackDoesNothingIfActionIsNull()
+        {
+            JetFetcher fetcher = new JetFetcher(null);
+            JToken token = JToken.FromObject(42);
+
+            Assert.DoesNotThrow(() => fetcher.CallFetchCallback(token));
+        }
+    }
+}
