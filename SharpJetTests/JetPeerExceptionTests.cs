c5b8813bc0cd9cfbe6a4445c5356d07dbce58994
Jens Eisenbach <jens.eisenbach@hbm.com>
2017-05-04 14:08:10 +0200

    Unit tests for JetPeerException

--- SharpJetTests/JetPeerExceptionTests.cs
+++ SharpJetTests/JetPeerExceptionTests.cs
@@ -28,37 +28,20 @@
 //
 // </copyright>
 
+using Hbm.Devices.Jet;
+
 namespace SharpJetTests
 {
-    using Hbm.Devices.Jet;
-    using Newtonsoft.Json.Linq;
     using NUnit.Framework;
 
     [TestFixture]
-    public class JsonRpcExceptionTests
+    public class JetPeerExceptionTests
     {
         [Test]
-        public void TestGetJsonWithoutData()
+        public void TestGetMessage()
         {
-            JsonRpcException exception = new JsonRpcException(42, "msg");
-            JObject json = exception.GetJson();
-            Assert.AreEqual(42, (int)json["code"]);
-            Assert.AreEqual("msg", (string)json["message"]);
-            Assert.IsNull(json["data"]);
-        }
-
-        [Test]
-        public void TestGetJsonWithData()
-        {
-            JObject data = new JObject();
-            data["d1"] = 42;
-            data["d2"] = "42";
-            JsonRpcException exception = new JsonRpcException(42, "msg", data);
-            JObject json = exception.GetJson();
-            Assert.AreEqual(42, (int)json["code"]);
-            Assert.AreEqual("msg", (string)json["message"]);
-            Assert.AreEqual(42, (int)json["data"]["d1"]);
-            Assert.AreEqual("42", (string)json["data"]["d2"]);
+            JetPeerException exception = new JetPeerException("msg");
+            Assert.AreEqual("msg", exception.Message);
         }
     }
 }
